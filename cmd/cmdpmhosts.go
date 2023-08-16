29b85fc5ed056725d67c479c6e607f8ecb613a4d
Eric Debeij <hdebeij@akamai.com>
2023-08-16 23:18:14 +0200

    doc

--- cmd/cmdpmhosts.go
+++ cmd/cmdpmhosts.go
@@ -12,9 +12,9 @@ import (
 // pmhostsCmd represents the hostlist command
 var pmhostsCmd = &cobra.Command{
 	Use:   "pmhosts",
-	Short: "List of all hostnames in your account per property",
+	Short: "List of all hostnames in your account per property with dns and certificate information",
 	Long: `An overview of the properties and the hostnames associated within the property. In order to find this information the property manager hostnames are downloaded (and stored in a cache).
-The related edgehost is shown and the host is checked to see if it is actually served by Akamai and resolves in a proper IP-address`,
+The related edgehost is shown and the host is checked to see if it is actually served by Akamai, resolves in a proper IP-address and information regarding the certificate being used`,
 	Run: func(cmd *cobra.Command, args []string) {
 		hr := &propreport.HostReport{
 			Export: viper.GetString("properties.hosts"),
