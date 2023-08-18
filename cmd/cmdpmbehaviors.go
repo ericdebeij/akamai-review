0e59e11c473a1cbb301e4622a0f53a5c21380520
Eric Debeij <hdebeij@akamai.com>
2023-08-18 13:23:48 +0200

    pm-behavior added, bug fixes, prepare version 0.1.2

---
+++ cmd/cmdpmbehaviors.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/ericdebeij/akamai-review/v3/report/propreport"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// originlistCmd represents the originlist command
+var pmbehaviorsCmd = &cobra.Command{
+	Use:   "pm-behaviors",
+	Short: "An overview of the behaviors in a propery",
+	Long:  `An overview of the properties and the behaviors implemented or details about a specific behavior`,
+	Run: func(cmd *cobra.Command, args []string) {
+		or := &propreport.BehaviorReport{
+			Export:   viper.GetString("pm-behaviors.export"),
+			Group:    viper.GetString("pm-behaviors.group"),
+			Property: viper.GetString("pm-behaviors.property"),
+			Behavior: viper.GetString("pm-behaviors.behavior"),
+		}
+		or.Report()
+	},
+}
+
+func init() {
+	param(pmbehaviorsCmd, "export", "pm-behaviors.export", "pm-behaviors.csv", "name of the exportfile")
+	param(pmbehaviorsCmd, "group", "pm-behaviors.group", "", "filter for the group")
+	param(pmbehaviorsCmd, "property", "pm-behaviors.property", "", "filter for the property")
+	param(pmbehaviorsCmd, "behavior", "pm-behaviors.behavior", "", "provide details about the specific behavior")
+	rootCmd.AddCommand(pmbehaviorsCmd)
+}
---
+++ todo.md
@@ -0,0 +1,8 @@
+# Todo
+
+The following topics are on the wishlist
+- Add review summary for every report, not just a csv
+- Aliases for products, contracts 
+- Usage reports with reporting groups
+- Support for other output types
+- Use golang features currently underutilized, concurrency (performance), interfaces (dependancies) 
