5d672ac6f89e554c2717a5d26534cf097c0053fa
Eric Debeij <hdebeij@akamai.com>
2023-11-10 09:42:11 +0100

    allow extra time for file closure

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ Copyright © 2022, 2023 NAME HERE hdebeij@akamai.com
 package main
 
 import (
+	"time"
+
 	"github.com/ericdebeij/akamai-review/v3/cmd"
 )
 
 func main() {
 	cmd.Execute()
 	cmd.Cleanup()
+
+	// giving time to close all files correctly
+	time.Sleep(time.Second * 3)
 }
