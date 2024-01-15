80cfdd7440343ec0bce9087d5ca855ee693cdccb
Eric Debeij <hdebeij@akamai.com>
2024-01-15 08:51:37 +0100

    automate version identification in build

--- patch.sh
+++ patch.sh
@@ -1,1 +1,5 @@
+#!/bin/sh
+VERSION="latest-$(date +%Y%m%dT%H%M%S)"
+sed "s/VERSION/${VERSION}/" cmd/version.template >cmd/version.go 
 go build -o patch/akamai-review && cp patch/akamai-review ~/.akamai-cli/src/akamai-review/akamai-review
+sed "s/VERSION/latest/" cmd/version.template >cmd/version.go 
